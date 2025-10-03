8dc4e7781ae8d006b33530b00383cb8b22b4327e
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-10-02 17:46:18 -0700

    Implementing some member functions in Movie.h

--- Movie.h
+++ Movie.h
@@ -54,8 +54,39 @@ public:
     }
 
     // Member functions
-    void addReview(double rating)
+    /*
+        addReview()
+        Add a MovieReview node to the head of the Linked list
+        Arguments:
+            - rating: a float value as the rating of the Movie (from 1.0 to 5.0)
+            - comment: a string as the comment on the Movie
+        Return: none
+    */
+    void addReview(double rating, string comment)
     {
+        // Declare a new MovieReview node
+        MovieReview* newNode = new MovieReview;
+        newNode->rating = rating;          // Assign the rating
+        newNode->comment = comment;        // Assign the comment
+        newNode->next = nullptr;           // Let next point to a nullptr
 
+        // Add the new MovieReview node to the head of the Linked list
+        newNode->next = reviews;
+        reviews = newNode;
+    }
+
+    void displayMovie()
+    {
+        // Display the title of the Movie
+        cout << " --- Title: " << getTitle() << " --- " << endl;
+
+        // Declare a new MovieReview node to traverse through the Linked list
+        MovieReview* current = new MovieReview;
+
+        // Display all the ratings and comments
+        while (current)
+        {
+
+        }
     }
 };
