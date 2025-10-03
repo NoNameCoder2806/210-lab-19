d6a6747654186c539a0113ce3ab44f37427c5b26
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-10-02 17:15:19 -0700

    Copied Lab 18 into the folder

---
+++ MovieReview.h
@@ -0,0 +1,15 @@
+// COMSC-210 | Lab 19 | Dat Hoang Vien
+// IDE used: Visual Studio Code
+
+// Libraries
+#include <iostream>
+#include <string>
+using namespace std;
+
+// MovieReview struct
+struct MovieReview
+{
+    float rating;             // The movie review's rating
+    string comment;           // The movie review's comment
+    MovieReview* next;        // The pointer to the next MovieReview node
+};
